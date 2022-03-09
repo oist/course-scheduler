89d76bce073b63702c07c872a769b4b7cdffc8bb
Jérémie Gillet <jie.gillet@gmail.com>
2022-03-09 15:34:46 +0900

    Remove warning

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 FILE=$3
-WARNING=`echo $FILE | sed s/schedule.csv/warnings.txt/` 
+# WARNING=`echo $FILE | sed s/schedule.csv/warnings.txt/` 
 PDF=`echo $FILE | sed s/.csv/_GS.pdf/` 
 
 /usr/local/bin/python3 Scheduler.py $1 $2 $3 > /dev/null 
 
-echo "" 
-
-cat $WARNING 
+# echo "" 
+# 
+# cat $WARNING 
 
 echo "" 
 
